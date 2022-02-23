a44ed3a0238e5ad1a2044e568d138d4cb772bcf4
brenodacosta <bdacosta@vicomtech.org>
2022-02-23 14:38:52 +0100

    Creating a video recognition webservice
    To be implemented

--- FlaskDevelopment/run2.py
+++ FlaskDevelopment/run2.py
@@ -12,51 +12,48 @@ plt = system()
 if plt == 'Linux': WindowsPath = PosixPath
 
 path = Path()
-learn_inf = load_learner(path/'export.pkl', cpu=True)
-btn_upload = widgets.FileUpload()
-out_pl = widgets.Output()
+learn_inf = load_learner(path/'export1.pkl', cpu=True)
+# btn_upload = widgets.FileUpload()
+# out_pl = widgets.Output()
 lbl_pred = widgets.Label()
-
 
 app = Flask(__name__)
 
-@app.route('/success/<name>')
+@app.route('/success/<pic>')
 def success(pic):
+    print(type(pic))
     lbl_pred.value = ''
-    img = PILImage.create(pic[-1])
-    out_pl.clear_output()
+    img = pic
+    # out_pl.clear_output()
+    print('\n\n\n llego aqui 2',img,'\n\n\n' )
     # with out_pl: display(img.to_thumb(128,128))
     pred,pred_idx,probs = learn_inf.predict(img)
+    print('\n\n\n llego aqui 3\n\n\n' )
     lbl_pred.value = f'Prediction: {pred}; Probability: {probs[pred_idx]:.04f}'
-
+    print(lbl_pred.value)
     return '%s' % lbl_pred.value
 
 @app.route('/login',methods = ['POST', 'GET'])
 def login():
     if request.method == 'POST':
-        # print(request.files)
-        # # print(request.files['imgInp'])
-        # picture = request.files.get('imgInp')
-        # img = Image.open(picture)
-        # display(img.to_thumb(128,128))
 
         filestr = request.files['imgInp'].read()
         npimg = np.fromstring(filestr, np.uint8)
-        # print(npimg)
         picture = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
-        picture = cv2.cvtColor(picture, cv2.COLOR_RGB2BGR)
-        # print(picture)
-        img = Image.fromarray(picture, 'RGB')
-        img.show()
-        # # img = PILImage.create(picture[-1])
-        # print('\n\nllego aqui 2',img,'\n\n')
-        # # out_pl.clear_output()
-        # print('Picture and img variables:',picture,img)
-        # with out_pl: display(img.to_thumb(128,128))
-        # return redirect(url_for('success',name = picture))
+        # picture = cv2.cvtColor(picture, cv2.COLOR_RGB2BGR)
+
+        img = PILImage.create(picture)
+        pred,pred_idx,probs = learn_inf.predict(img)
+        lbl_pred.value = f'Prediction: {pred}; Probability: {probs[pred_idx]:.04f}'
+        print(lbl_pred.value,probs)
+
+        return '%s' % lbl_pred.value
+
+        # return redirect(url_for('success',pic = img))
     else:
+        print('here')
         user = request.args.get('nm')
-        return redirect(url_for('success',name = user))
+        return redirect(url_for('success',pic = user))
 
 if __name__ == '__main__':
     app.run(debug = True)
